f614204b0de8215334a6b98f1dcb6ace6a4cf190
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-08-25 22:21:15 +0200

    Included today's date in answer

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const btnSubmit = document.querySelector(".submit_btn");
 const textReturned = document.querySelector(".text_in");
 const returnAnswer = document.querySelector(".answer");
 paragraph.textContent += " This just got added";
-
+const formatDate = function (date) {
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-
-  returnAnswer.textContent = "Your answer: " + textReturned.value.toUpperCase();
+  const today = new Date();
+  const dateStr = formatDate(today);
+  returnAnswer.textContent = `Your answer: on ${dateStr}: ${textReturned.value.toUpperCase()}`;
 });
