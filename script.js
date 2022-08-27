ace9a82b2fbd838b01fa56813a9af542e4913fd1
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-08-27 06:59:27 +0200

    added work with dates, arrays and arrow fn

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ const btnSubmit = document.querySelector(".submit_btn");
 const textReturned = document.querySelector(".text_in");
 const returnAnswer = document.querySelector(".answer");
 paragraph.textContent += " This just got added";
-const formatDate = function (date) {
-  const day = `${date.getDate()}`.padStart(2, "0");
-  const month = `${date.getMonth() + 1}`.padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-};
+
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-  const today = new Date();
-  const dateStr = formatDate(today);
+  
+  const dateStr = new Intl.DateTimeFormat('en-SE').format(new Date());
   returnAnswer.textContent = `Your answer: on ${dateStr}: ${textReturned.value.toUpperCase()}`;
 });
+const arr = [3,7,12,4,63,128]
+console.log(arr.map(a => a * 2))
